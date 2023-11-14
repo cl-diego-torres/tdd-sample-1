9e59810f41ffdfe458f653b9c0e257e8e16f31f0
diego.torres <cl.diego.torres@gmail.com>
2023-11-13 22:08:40 -0300

    Initial commit

---
+++ src/main/java/cl/dtorres/tddsample1/TddSample1Application.java
@@ -0,0 +1,13 @@
+package cl.dtorres.tddsample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddSample1Application.class, args);
+	}
+
+}
---
+++ src/test/java/cl/dtorres/tddsample1/TddSample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.dtorres.tddsample1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddSample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
